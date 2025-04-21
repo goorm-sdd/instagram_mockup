5df8ae008dd28d7dabb7943b46873fa6d52f6f3b
cherish0-0 <jiyu01160@naver.com>
2025-04-22 00:23:14 +0900

    feat(story): 스토리 이미지 스크롤 구현 및 동일한 이미지 겹쳐 나오지 않도록 수정

--- js/StoryDetail.js
+++ js/StoryDetail.js
@@ -1,44 +1,70 @@
 import { posts } from "../data/mockData.js";
 
 const images = [];
+const addedImages = new Set();
 
 const shuffled = posts.sort(() => 0.5 - Math.random());
 
 for (const post of shuffled) {
-  images.push(post.image);
+  if (!addedImages.has(post.image)) {
+    images.push(post.image);
+    addedImages.add(post.image);
+  }
 
   if (images.length >= 3) break;
 }
 
-let currentIndex = 0;
+console.log(images);
 
-const storyImage = document.querySelector(".story-images img");
+const storyImages = document.querySelector(".story-images");
+
+for (let i = 0; i < images.length; i++) {
+  const storyImage = document.createElement("div");
+  storyImage.className = "story-image";
+
+  const image = document.createElement("img");
+  image.src = images[i];
+
+  storyImages.appendChild(storyImage);
+  storyImage.appendChild(image);
+}
+
+console.log(storyImages);
+
 const arrowLeft = document.querySelector(".arrow-left");
 const arrowRight = document.querySelector(".arrow-right");
 
-if (images.length > 0) {
-  storyImage.src = images[currentIndex];
-}
-
-let timeoutId;
-
-function resetTimer() {
-  clearTimeout(timeoutId);
-  timeoutId = setTimeout(() => {
-    window.location.href = "../html/index.html";
-  }, 4000);
-}
-
 arrowLeft.addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  storyImage.src = images[currentIndex];
-  resetTimer();
+  document.querySelector(".story-images").scrollLeft -= window.innerWidth - 80;
 });
 
-arrowRight.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % images.length;
-  storyImage.src = images[currentIndex];
-  resetTimer();
-});
+// let currentIndex = images[0];
 
-resetTimer();
+// const storyImage = document.querySelector(".story-images img");
+
+// if (images.length > 0) {
+//   storyImage.src = currentIndex;
+// }
+
+// let timeoutId;
+
+// function resetTimer() {
+//   clearTimeout(timeoutId);
+//   timeoutId = setTimeout(() => {
+//     window.location.href = "../html/index.html";
+//   }, 4000);
+// }
+
+// arrowLeft.addEventListener("click", () => {
+//   currentIndex = (currentIndex - 1 + images.length) % images.length;
+//   storyImage.src = images[currentIndex];
+//   // resetTimer();
+// });
+
+// arrowRight.addEventListener("click", () => {
+//   currentIndex = (currentIndex + 1) % images.length;
+//   storyImage.src = images[currentIndex];
+//   // resetTimer();
+// });
+
+// // resetTimer();
