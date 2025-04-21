d672ea78e41ed71581f2be7638611ca429978f37
cherish0-0 <jiyu01160@naver.com>
2025-04-22 01:38:52 +0900

    feat(story): 주석 처리했던 timer 재구현

--- js/StoryDetail.js
+++ js/StoryDetail.js
@@ -29,7 +29,14 @@ for (let i = 0; i < images.length; i++) {
   storyImage.appendChild(image);
 }
 
-console.log(storyImages);
+let timeoutId;
+
+function resetTimer() {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    window.location.href = "../html/index.html";
+  }, 4000);
+}
 
 const arrowLeft = document.querySelector(".arrow-left");
 const arrowRight = document.querySelector(".arrow-right");
@@ -42,6 +49,7 @@ arrowLeft.addEventListener("click", () => {
   storyImages.scrollTo({
     left: currentPosition - imageWidth,
   });
+  resetTimer();
 });
 
 arrowRight.addEventListener("click", () => {
@@ -51,35 +59,7 @@ arrowRight.addEventListener("click", () => {
   storyImages.scrollTo({
     left: currentPosition + imageWidth,
   });
+  resetTimer();
 });
 
-// let currentIndex = images[0];
-
-// const storyImage = document.querySelector(".story-images img");
-
-// if (images.length > 0) {
-//   storyImage.src = currentIndex;
-// }
-
-// let timeoutId;
-
-// function resetTimer() {
-//   clearTimeout(timeoutId);
-//   timeoutId = setTimeout(() => {
-//     window.location.href = "../html/index.html";
-//   }, 4000);
-// }
-
-// arrowLeft.addEventListener("click", () => {
-//   currentIndex = (currentIndex - 1 + images.length) % images.length;
-//   storyImage.src = images[currentIndex];
-//   // resetTimer();
-// });
-
-// arrowRight.addEventListener("click", () => {
-//   currentIndex = (currentIndex + 1) % images.length;
-//   storyImage.src = images[currentIndex];
-//   // resetTimer();
-// });
-
-// // resetTimer();
+resetTimer();
