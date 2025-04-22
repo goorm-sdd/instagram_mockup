a8f1ccf333dceb91455e042d9523e95547c9d347
최범근_한국외국어대학교 <87818539+vayaconChoi@users.noreply.github.com>
2025-04-22 13:18:14 +0900

    Revert "Fix/profile"

--- js/IncludeFooter.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const placeholder = document.getElementById("footer-placeholder");
-  fetch("../html/Footer.html")
-    .then((res) => res.text())
-    .then((data) => {
-      placeholder.innerHTML = data;
-    });
-});
--- js/include.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const placeholder = document.getElementById("header-placeholder");
-  fetch("../html/header.html")
-    .then((res) => res.text())
-    .then((data) => {
-      placeholder.innerHTML = data;
-    });
-});
